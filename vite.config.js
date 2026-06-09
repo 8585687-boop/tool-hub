2b188665694d9e0eba8088340c7fa4181be5a558
8585687-boop <8585687@gmail.com>
2026-06-09 16:36:41 +0800

    auto generate sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
+
 
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+
+plugins:[
+
+react(),
+
+Sitemap({
+
+hostname:
+'https://tool-hub-bxb.pages.dev'
+
+})
+
+]
+
 })
